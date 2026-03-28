356fcddb8a9a29337ab7e2e50a116da106934d17
Ghost <75737627+SebastianKoehler@users.noreply.github.com>
2026-03-28 23:41:23 +0100

    chore: initialize solution structure with domain layer and base projects

---
+++ src/PasswordGenerator.Domain/Entities/PasswordOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace PasswordGenerator.Domain.Entities;
+
+public class PasswordOptions
+{
+    public int Length { get; set; } = 12;
+    public bool UseUppercase { get; set; } = true;
+    public bool UseLowercase { get; set; } = true;
+    public bool UseDigits { get; set; } = true;
+    public bool UseSpecialCharacters { get; set; } = true;
+}
---
+++ src/PasswordGenerator.Domain/Interfaces/IPasswordGenerator.cs
@@ -0,0 +1,8 @@
+﻿using PasswordGenerator.Domain.Entities;
+
+namespace PasswordGenerator.Domain.Interfaces;
+
+public interface IPasswordGenerator
+{
+    string Generate(PasswordOptions options);
+}
